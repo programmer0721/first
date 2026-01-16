f07ffac318ce87ce08636e5833c84f15b9837a80
programmer0721 <2642583636@qq.com>
2026-01-16 17:59:21 +0800

    Achange

--- 输出数字.cpp
+++ 输出数字.cpp
@@ -2,7 +2,7 @@
 int i=0;
 void print(){
 	printf("%d\n",i++);
-	while(i<=352) print();//我是B，把350改成351 //A又把351改成了352
+	while(i<=352) print();//我是B，把350改成351 //A又把351改成了352//A
 }
 int main(){
 	print();
